80428963f0a2066903eca6f1b387ceb7a5469008
Aiden Adkisson <135772591+Mak3rT0n@users.noreply.github.com>
2025-05-27 20:07:17 -0700

    Fully functioning proof of concept, spotify to JSON only
    This is fully functioning to playlists, albums and radios seem to work, but I am expecting them not to in some cases.
    
    Changes include
    - Flexible xpaths, all xpaths shouldnt be affected by the inconsistent nature of spotify's dom
    - Just paste the link to the playlist now, I should have done it sooner
    -Cleaned up some unused code
    -Added the TODO list of items
    -Added some "unit tests" that sometimes dont print if the selenium window isnt up.

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-import selenium
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import json
 
+#TODO Add GUI to paste in links
+#TODO Add progress bar into GUI
+#TODO Funcionize code
+#TODO DRY code
+#TODO Switch to CSV and convert file type from the GUI into what ever file type selected
+#TODO Add ability to auto download a zip with all of the songs (aiming for functionality will add higher quality like FLAC after functional)
+
 #Any Spotify playlist or radio as long as it's public
 #will be scraped if added here
-TargetUrl = 'https://open.spotify.com/playlist/4TI52y8ILsaz1ae9DjS9ns'
+print("Paste or type your playlist: ")
+TargetUrl = str(input())
 '''TargetUrl = 'spotify url' '''
 
 driver = webdriver.Firefox()
 
 iteration = 0
-
-SCROLL_PAUSE_TIME = 1.5
 
 
 #Try block holds all code for scraping the data and adding it to a json file
@@ -25,7 +30,7 @@ try:
         '//div[@class="RP2rRchy4i8TIp1CTmb7"]/span[2]'))
     )
     uncleaned_playlist_name = playlist_name_xpath.text
-
+    print("got playlist name")
     #Cleans the playlist name for the creation of the json file
     cleaned_playlist_name = ''
     for i in uncleaned_playlist_name:
@@ -33,19 +38,19 @@ try:
             cleaned_playlist_name += "_"
         else:
             cleaned_playlist_name += i
-
+    print("cleaned playlist name")
     playlist_song_amount_xpath = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, 
-        '//div[@class="RP2rRchy4i8TIp1CTmb7"]/div/div[2]/span[1]'))
+        '//span[@data-testid="entityTitle"]/following-sibling::*[last()]/div[last()]/span'))
     )
     playlist_song_amount = playlist_song_amount_xpath.text
-
+    print("got song amount")
     #resolved having the original comma and "songs" in the number 
     num_string = playlist_song_amount.split()[0]
     cleaned_num = num_string.replace(",", "")
     cleaned_playlist_song_amount = int(cleaned_num)
     
     total_songs_expected = cleaned_playlist_song_amount
-
+    print(total_songs_expected)
     playlist_data = {
             "Playlist Name" : uncleaned_playlist_name,
             "Playlist Song Amount" : playlist_song_amount,
@@ -62,8 +67,9 @@ try:
         
         print("Started Scraping")
 
-        # +1 is needed because the first song of the row starts at row 2
-        for i in range(1, (total_songs_expected + 1)):
+        #the html row has the first song at row 2, we add 2 since the end is excluded
+        #Eg. song 1 is located on row 2 so we need a range of 3
+        for i in range(1, (total_songs_expected + 2)):
             try:                
                 iteration += 1
                 
@@ -77,8 +83,8 @@ try:
                 
                 song_number = str(iteration - 1)
                 song_name = current_row.find_element(By.XPATH, './/a/div').text
-                artist_name = current_row.find_element(By.XPATH, './/a/following-sibling::span/div/a').text
-                song_length = current_row.find_element(By.XPATH, './div/div[5]/div').text
+                artist_name = current_row.find_element(By.XPATH, './/div[@role="gridcell" and @aria-colindex="2"]//span[last()]//a').text
+                song_length = current_row.find_element(By.XPATH, './/div[@role="gridcell"][last()]/div[@data-encore-id="text"]').text
             
                 song_data = {
                     "No." : song_number,
